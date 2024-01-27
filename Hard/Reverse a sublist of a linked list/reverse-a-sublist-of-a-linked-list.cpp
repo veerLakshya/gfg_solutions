21f54ad4b9c50e1be5c9f386fc5e625727c92cfc
veerLakshya <109882583+veerLakshya@users.noreply.github.com>
2024-01-27 06:51:30 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Reverse a sublist of a linked list/reverse-a-sublist-of-a-linked-list.cpp
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*Link list node 
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};*/
+
+class Solution
+{
+    public:
+    Node* reverseBetween(Node* head, int m, int n)
+    {
+        m--; n--;
+        if(head==NULL ||head->next== NULL) return head;
+        vector <int> arr;
+        Node* temp = head;
+        while(temp)
+        {
+            arr.push_back(temp->data);
+            temp=temp->next;
+        }
+        while(m<n)
+        {
+            swap(arr[m],arr[n]);
+            m++;n--;
+        }
+        temp=head;
+        for(auto i: arr)
+        {
+            temp->data =i;
+            temp = temp->next;
+        }
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Function to print linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
+
+
+
+// Driver program to test above functions
+int main()
+{
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int N, m, n;
+		cin >> N>>m>>n;
+
+		Node *head = NULL;
+		Node *temp = head;
+
+		for (int i = 0; i < N; i++) {
+			int data;
+			cin >> data;
+			if (head == NULL)
+				head = temp = new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+
+		
+
+        Solution ob;
+
+		Node* newhead = ob.reverseBetween(head, m, n);
+		printList(newhead);
+
+		cout << "\n";
+
+
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends
