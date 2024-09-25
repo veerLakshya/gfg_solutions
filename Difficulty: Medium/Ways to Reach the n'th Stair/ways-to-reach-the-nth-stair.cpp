bcf97f3aa928a55679134fcc39de575c30bf3a6b
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2024-09-25 21:40:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.cpp
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.cpp
@@ -11,8 +11,13 @@ class Solution
     {
         int mod = 1e9 + 7;
         int dp[n+1];
-        dp[0]=1, dp[1]=1;
-        for(int i = 2; i <= n; i++) dp[i] = dp[i-1]+dp[i-2], dp[i] %= mod;
+        dp[1] = 1;
+        dp[0] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] %= mod;
+        }
         return dp[n];
     }
 };
