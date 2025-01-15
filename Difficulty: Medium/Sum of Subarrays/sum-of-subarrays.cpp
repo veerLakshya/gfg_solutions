f5ca9b6f5620afd52df24e3eae016c7ef75776b8
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-01-15 17:49:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Subarrays/sum-of-subarrays.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    long long subarraySum(vector<int>& arr) {
+        long long ans = 0, n = arr.size(), mod = 1e9 + 7;
+        for(int i = 0; i < n; i++){
+            int el = arr[i];
+            ans += ((i+1)*(n-i)*arr[i]);
+            ans %= mod;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        long long res = obj.subarraySum(arr);
+        cout << res << endl;
+        cout << "~" << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
